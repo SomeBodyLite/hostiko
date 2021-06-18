31c0f52a82fac968a95dcd317add59b568274d48
SomeBodyLite <78022230+SomeBodyLite@users.noreply.github.com>
2021-06-19 01:45:19 +0600

    Свёрстано фильтры

--- assets/js/dedic-filters.js
+++ assets/js/dedic-filters.js
@@ -23,3 +23,30 @@ $(document).ready(function () {
   //Эмуляция клика по кнопке
   // $(".tabs-2-triggers__item:first").click();
 });
+
+let select = function () {
+  let selectHeader = document.querySelectorAll(".select__header");
+  let selectItem = document.querySelectorAll(".select__item");
+
+  selectHeader.forEach((item) => {
+    item.addEventListener("click", selectToggle);
+  });
+
+  selectItem.forEach((item) => {
+    item.addEventListener("click", selectChoose);
+  });
+
+  function selectToggle() {
+    this.parentElement.classList.toggle("is-active");
+  }
+
+  function selectChoose() {
+    let text = this.innerText;
+    select = this.closest(".select");
+    currentText = select.querySelector(".select__current");
+    currentText.innerText = text;
+    select.classList.remove("is-active");
+  }
+};
+
+select();
