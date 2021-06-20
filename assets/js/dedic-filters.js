244e7946049c1ab22dc3d37862e992ba001c0667
SomeBodyLite <78022230+SomeBodyLite@users.noreply.github.com>
2021-06-20 22:33:28 +0600

    Много изменений

--- assets/js/dedic-filters.js
+++ assets/js/dedic-filters.js
@@ -1,28 +1,18 @@
-$(document).ready(function () {
-  $(".tabs-2-triggers__item").click(function (e) {
-    e.preventDefault();
-
-    $(".tabs-2-triggers__item").removeClass("tabs-2-triggers__item--active");
-    $(".tabs-2-content__item").removeClass("tabs-2-content__item--active");
-
-    $(this).addClass("tabs-2-triggers__item--active");
-    $($(this).attr("href")).addClass("tabs-2-content__item--active");
-  });
-  // Вкл/выкл
-  $(".tabs-2-triggers__item--active").click(function (e) {
-    e.preventDefault();
-
-    $(".tabs-2-triggers__item--active").removeClass("tabs-2-triggers__item");
-    $(".tabs-2-content__item--active").removeClass("tabs-2-content__item");
-    console.log("Сработало");
-
-    $(this).removeClass("tabs-2-triggers__item--active");
-    $($(this).attr("href")).removeClass("tabs-2-content__item--active");
-  });
-
-  //Эмуляция клика по кнопке
-  // $(".tabs-2-triggers__item:first").click();
-});
+function viewDiv() {
+  if (document.getElementById("tab-2-1").style.display == "block") {
+    document.getElementById("tab-2-1").style.display = "none";
+    document
+      .querySelectorAll(".tabs-2-triggers__item")
+      .forEach((child) =>
+        child.classList.remove("tabs-2-triggers__item--active")
+      );
+  } else {
+    document.getElementById("tab-2-1").style.display = "block";
+    document
+      .querySelectorAll(".tabs-2-triggers__item")
+      .forEach((child) => child.classList.add("tabs-2-triggers__item--active"));
+  }
+}
 
 let select = function () {
   let selectHeader = document.querySelectorAll(".select__header");
