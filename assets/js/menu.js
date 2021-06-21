a0e08a23fe25b5cb9db12780e6fc3ee50a692eee
SomeBodyLite <78022230+SomeBodyLite@users.noreply.github.com>
2021-06-21 20:48:25 +0600

    Проверка дропдаун контента

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -7,3 +7,17 @@ $(document).ready(function () {
     $(".header-nav").toggleClass("flex-column");
   });
 });
+
+function viewDiv() {
+  if (document.getElementById("m-drop").style.display == "block") {
+    document.getElementById("m-drop").style.display = "none";
+    document
+      .querySelectorAll(".dropdown-content")
+      .forEach((child) => child.classList.remove("dropdown-content--active"));
+  } else {
+    document.getElementById("m-drop").style.display = "block";
+    document
+      .querySelectorAll(".dropdown-content")
+      .forEach((child) => child.classList.add("dropdown-content--active"));
+  }
+}
